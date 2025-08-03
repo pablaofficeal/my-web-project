d6b8383512182fa19c018ae00a140666b165f176
pablaofficeal <pavlogorosko56@gmail.com>
2025-08-03 23:14:29 +0200

    Remove web_platform_win blueprint and related file; update index.html to use CSS variables for colors and add favicon link.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
 from config import Config
 from models import db
-from web_platform_win import web_platform_win
 from routers.home import home_host
 
 
@@ -9,7 +8,6 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 
-app.register_blueprint(web_platform_win)
 app.register_blueprint(home_host)
 
 db.init_app(app)
